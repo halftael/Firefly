7b287706e5c1f926626040fd696b0cc044f28e36
halftael <halftael@gmail.com>
2026-06-29 21:07:01 +0800

    update: config init for myself

--- src/config/announcementConfig.ts
+++ src/config/announcementConfig.ts
@@ -5,7 +5,7 @@ export const announcementConfig: AnnouncementConfig = {
 	title: "公告",
 
 	// 公告内容
-	content: "欢迎来到我的博客！这是一则示例公告。",
+	content: "本博客内容主要服务于本人，内容仅供参考..?",
 
 	// 是否允许用户关闭公告
 	closable: true,
